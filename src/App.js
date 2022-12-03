9446b72001472541d9662ee67bbcdd8f87eb992d
Olha Podorozhnaya <91029349+OlgaPodorozhnaya@users.noreply.github.com>
2022-12-02 17:50:54 -0800

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather APP
+      <h1>Weather APP</h1>
+      <span>This project was coded by Olha Podorozhna </span>
+
+      <a href="https://github.com/OlgaPodorozhnaya/weather-react" target="_blank" rel="noreferrer"> GitHub</a>
     
     </div>
   );
