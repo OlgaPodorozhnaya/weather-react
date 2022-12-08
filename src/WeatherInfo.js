4ff232a533172dc573981c6428c0802e947be5e6
Olha Podorozhnaya <91029349+OlgaPodorozhnaya@users.noreply.github.com>
2022-12-07 21:13:14 -0800

    Created a new component that converts Celsius to Fahrenheit  and vice versa

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
 
 export default function WeatherInfo (props) {
     return (<div className="WeatherInfo" >
@@ -15,12 +17,13 @@ export default function WeatherInfo (props) {
     <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-             <div className="float-left ">
+             <span className="float-left ">
                 <WeatherIcon code={props.data.icon} />
-                
-           <span className="temperature ">{Math.round(props.data.temperature)}</span> 
-           <span className="unit">°C</span>
-           </div>
+                </span>
+              
+               <WeatherTemperature celsius={props.data.temperature}/> 
+             
+          
            </div>
         </div>
         <div className="col-6">
